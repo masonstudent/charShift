10fe90e8b710666e275b328e9cf248816f377aef
masonstudent <prongerm@myumanitoba.ca>
2022-12-01 11:37:33 -0600

    compleated the bit shift while loop.

--- charShift.java
+++ charShift.java
@@ -1,11 +1,24 @@
 class charshift{
     public static void main(String[] args) {
         char i = 'a';
+        int sum,carry;
 
-        System.out.println(i);
-
-        
+        //System.out.println(Integer.toBinaryString(a))
 
 
+        for(int j = 0;j<26;j++){
+            System.out.println(i);
+            carry = i & 1;
+            sum = i^1;
+            
+            while(carry != 0){
+                int shiftBit = carry << 1;
+                carry = sum & shiftBit;
+                sum = sum ^ shiftBit;
+            }
+            i = (char) sum; 
+        }
+            
+            
     }
 }
