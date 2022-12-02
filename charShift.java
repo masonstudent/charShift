7358ca6d6a0579ccbac1c1009bf67e95753e32ec
masonstudent <prongerm@myumanitoba.ca>
2022-12-02 10:59:44 -0600

    finished the function for print alphabet

--- charShift.java
+++ charShift.java
@@ -2,12 +2,12 @@ class charshift{
     public static void main(String[] args) {
         char i = 'a';
 
-        System.out.println(i);
+        printAlphabet(i);
 
     
 
     }
-    public void printAlphabet(char i){
+    public static void printAlphabet(char i){
         int sum,carry;
         for(int j = 0;j<26;j++){
             System.out.println(i);
