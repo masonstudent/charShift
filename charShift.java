20f3504068ac037a5950cfd9cca515621e29d76a
masonstudent <prongerm@myumanitoba.ca>
2022-12-02 10:55:21 -0600

    added printAlphabet function

--- charShift.java
+++ charShift.java
@@ -1,24 +1,27 @@
 class charshift{
     public static void main(String[] args) {
         char i = 'a';
+        char j = 'A';
+
+        //System.out.println(Integer.toBinaryString(a))   
+        printAlphabet(i); 
+        printAlphabet(j);   
+    }
+
+    public static void printAlphabet(char start){
         int sum,carry;
-
-        //System.out.println(Integer.toBinaryString(a))
-
-
         for(int j = 0;j<26;j++){
-            System.out.println(i);
-            carry = i & 1;
-            sum = i^1;
+            System.out.println(start);
+            carry = start & 1;
+            sum = start^1;
             
             while(carry != 0){
                 int shiftBit = carry << 1;
                 carry = sum & shiftBit;
                 sum = sum ^ shiftBit;
             }
-            i = (char) sum; 
+            start = (char) sum; 
         }
-            
-            
+
     }
 }
