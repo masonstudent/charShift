de63c64b0510a602d93f37ff6f72b2d889c93ade
masonstudent <prongerm@myumanitoba.ca>
2022-12-02 10:58:13 -0600

    .

--- charShift.java
+++ charShift.java
@@ -4,8 +4,22 @@ class charshift{
 
         System.out.println(i);
 
-        
+    
 
-
+    }
+    public void printAlphabet(char i){
+        int sum,carry;
+        for(int j = 0;j<26;j++){
+            System.out.println(i);
+            carry = i & 1;
+            sum = i^1;
+            
+            while(carry != 0){
+                int shiftBit = carry << 1;
+                carry = sum & shiftBit;
+                sum = sum ^ shiftBit;
+            }
+            i = (char) sum; 
+        }
     }
 }
